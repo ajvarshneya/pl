d49c6e4b95b0809ec92c9ce96bc66da32f4014f4
aj <aj@ajs-MacBook-Air.local>
2016-02-08 00:17:03 -0500

    CA1 things, 14/16

--- ca1/main.py
+++ ca1/main.py
@@ -1,5 +1,7 @@
 
 import sys
+import copy
+
 class TACAssign(object):
 	def __init__(self, assignee, op1):
 		self.assignee = assignee
@@ -80,30 +82,30 @@ class TACEqual(object):
 
 
 class TACInt(object):
-	def __init__(self, assignee, op1):
+	def __init__(self, assignee, val):
 		self.assignee = assignee
-		self.op1 = op1
+		self.val = val
 
 	def __str__(self):
-		return str(self.assignee) + ' <- int ' + str(self.op1)
+		return str(self.assignee) + ' <- int ' + str(self.val)
 
 
 class TACBool(object):
-	def __init__(self, assignee, op1):
+	def __init__(self, assignee, val):
 		self.assignee = assignee
-		self.op1 = op1
+		self.val = val
 
 	def __str__(self):
-		return str(self.assignee) + ' <- bool ' + str(self.op1)
+		return str(self.assignee) + ' <- bool ' + str(self.val)
 
 
 class TACString(object):
-	def __init__(self, assignee, op1):
+	def __init__(self, assignee, val):
 		self.assignee = assignee
-		self.op1 = op1
+		self.val = val
 
 	def __str__(self):
-		return str(self.assignee) + ' <- string ' + str(self.op1)
+		return str(self.assignee) + ' <- string ' + str(self.val)
 
 
 class TACNot(object):
@@ -125,21 +127,21 @@ class TACNeg(object):
 
 
 class TACNew(object):
-	def __init__(self, assignee, op1):
+	def __init__(self, assignee, c_type):
 		self.assignee = assignee
-		self.op1 = op1
+		self.c_type = c_type
 
 	def __str__(self):
-		return str(self.assignee) + ' <- new ' + str(self.op1)
+		return str(self.assignee) + ' <- new ' + str(self.c_type)
 
 
 class TACDefault(object):
-	def __init__(self, assignee, op1):
+	def __init__(self, assignee, c_type):
 		self.assignee = assignee
-		self.op1 = op1
+		self.c_type = c_type
 
 	def __str__(self):
-		return str(self.assignee) + ' <- default ' + str(self.op1)
+		return str(self.assignee) + ' <- default ' + str(self.c_type)
 
 
 class TACIsVoid(object):
@@ -155,6 +157,7 @@ class TACOutInt(object):
 	def __init__(self, assignee, op1):
 		self.assignee = assignee
 		self.op1 = op1
+		self.call = True
 
 	def __str__(self):
 		return str(self.assignee) + ' <- call out_int ' + str(self.op1)
@@ -164,6 +167,7 @@ class TACOutString(object):
 	def __init__(self, assignee, op1):
 		self.assignee = assignee
 		self.op1 = op1
+		self.call = True
 
 	def __str__(self):
 		return str(self.assignee) + ' <- call out_string ' + str(self.op1)
@@ -172,6 +176,7 @@ class TACOutString(object):
 class TACInInt(object):
 	def __init__(self, assignee):
 		self.assignee = assignee
+		self.call = True
 
 	def __str__(self):
 		return str(self.assignee) + ' <- call in_int'
@@ -180,6 +185,7 @@ class TACInInt(object):
 class TACInString(object):
 	def __init__(self, assignee):
 		self.assignee = assignee
+		self.call = True
 
 	def __str__(self):
 		return str(self.assignee) + ' <- call in_string'
@@ -188,6 +194,7 @@ class TACInString(object):
 class TACAbort(object):
 	def __init__(self, assignee):
 		self.assignee = assignee
+		self.call = True
 
 	def __str__(self):
 		return str(self.assignee) + ' <- call abort'
@@ -210,19 +217,19 @@ class TACLabel(object):
 		return 'label ' + str(self.label)
 
 class TACReturn(object):
-	def __init__(self, ret):
-		self.ret = ret
+	def __init__(self, val):
+		self.val = val
 
 	def __str__(self):
-		return 'return ' + str(self.ret)
+		return 'return ' + str(self.val)
 
 class TACBt(object):
-	def __init__(self, boolean, label):
-		self.boolean = boolean
+	def __init__(self, val, label):
+		self.val = val
 		self.label = label
 
 	def __str__(self):
-		return 'bt ' + str(self.boolean) + ' ' + str(self.label)
+		return 'bt ' + str(self.val) + ' ' + str(self.label)
 
 ## BASIC BLOCK DEFINITION
 
@@ -234,6 +241,9 @@ class TACBasicBlock(object):
 		self.children = []
 		self.parents = []
 
+		self.live_in = set()
+		self.live_out = set()
+
 		self.child_labels = [inst.label for inst in self.insts 
 							if isinstance(inst, TACJmp) or
 							   isinstance(inst, TACBt)]
@@ -241,19 +251,15 @@ class TACBasicBlock(object):
 	def __str__(self):
 		s = 'Label : ' + str(self.label) + '\n'
 		s += 'Parents : ' + str([parent.label for parent in self.parents]) + '\n'
+		s += 'Children : ' + str([child.label for child in self.children]) + '\n'
+
 		for idx, item in enumerate(self.insts):
 			s += str(idx) + '. ' + str(item) + '\n'
-			s += 'Children : ' + str([child.label for child in self.children]) + '\n'
+
+		s += 'Live_in : ' + str([live_var for live_var in self.live_in]) + '\n'
+		s += 'Live_out : ' + str([live_var for live_var in self.live_out]) + '\n'
+
 		return s
-
-
-def read_tac(filename):
-	f = open(filename)
-	tac = []
-	for line in f:
-		tac.append(line.rstrip('\n').rstrip('\r'))
-	f.close()
-	return tac
 
 
 def make_inst_list(tac):
@@ -269,16 +275,16 @@ def make_inst_list(tac):
 
 		elif inst[0] == 'jmp': # Jump instruction
 			label = inst[1]
-			inst_list.append(TACLabel(label))
+			inst_list.append(TACJmp(label))
 
 		elif inst[0] == 'bt': # Branch instruction
-			boolean = inst[1]
+			val = inst[1]
 			label = inst[2]
-			inst_list.append(TACBt(boolean, label))
+			inst_list.append(TACBt(val, label))
 
 		elif inst[0] == 'return': # Return instruction
-			ret = inst[1]
-			inst_list.append(TACReturn(ret))
+			val = inst[1]
+			inst_list.append(TACReturn(val))
 
 		elif inst[0] == 'comment': # Comment instruction
 			continue
@@ -321,17 +327,17 @@ def make_inst_list(tac):
 				inst_list.append(TACEqual(assignee, op1, op2))
 
 			elif inst[2] == 'int': # Integer constant instruction
-				op1 = inst[3]
-				inst_list.append(TACInt(assignee, op1))
+				val = inst[3]
+				inst_list.append(TACInt(assignee, val))
 
 			elif inst[2] == 'bool': # Boolean constant instruction
-				op1 = inst[3]
-				inst_list.append(TACBool(assignee, op1))
+				val = inst[3]
+				inst_list.append(TACBool(assignee, val))
 			
 			elif inst[2] == 'string': # String constant instruction
 				inst = next(tac_iter) # Skip a line
-				op1 = inst  # Take first (and only) item
-				inst_list.append(TACString(assignee, op1))
+				val = inst  # Take first (and only) item
+				inst_list.append(TACString(assignee, val))
 
 			elif inst[2] == 'not': # Boolean negation instruction
 				op1 = inst[3]
@@ -342,12 +348,12 @@ def make_inst_list(tac):
 				inst_list.append(TACNot(assignee, op1))
 
 			elif inst[2] == 'new': # Object allocation instruction
-				op1 = inst[3]
-				inst_list.append(TACNew(assignee, op1))
+				c_type = inst[3]
+				inst_list.append(TACNew(assignee, c_type))
 
 			elif inst[2] == 'default': # Default assignment instruction
-				op1 = inst[3]
-				inst_list.append(TACDefault(assignee, op1))
+				c_type = inst[3]
+				inst_list.append(TACDefault(assignee, c_type))
 
 			elif inst[2] == 'isvoid': # Void check instruction
 				op1 = inst[3]
@@ -361,10 +367,8 @@ def make_inst_list(tac):
 					op1 = inst[4]
 					inst_list.append(TACOutString(assignee, op1))					
 				if inst[3] == 'in_int':
-					op1 = inst[4]
 					inst_list.append(TACInInt(assignee))
 				if inst[3] == 'in_string':
-					op1 = inst[4]
 					inst_list.append(TACInString(assignee))
 				if inst[3] == 'abort':
 					inst_list.append(TACAbort(assignee))
@@ -407,10 +411,86 @@ def make_bbs(insts):
 		for child in block.children:
 			child.parents += [block]
 
-	return blocks
+	return blocks 
 
-def liveness(block):
-	
+
+# Percolate live_out set up the block
+def kill_dead_code(block):
+	removal = False
+	live_set = copy.copy(block.live_out)
+
+ 	for inst in reversed(block.insts[:]):
+
+ 		# Branch/return cases, add to live set
+ 		if isinstance(inst, TACBt) or isinstance(inst, TACReturn):
+ 			live_set.add(inst.val)
+
+ 		# Assignee cases, remove assignee from live set
+ 		if hasattr(inst, 'assignee'):
+ 			if (inst.assignee in live_set) or hasattr(inst, 'call'):
+  				live_set.discard(inst.assignee)
+		 		if hasattr(inst, 'op1'):
+		 			live_set.add(inst.op1)
+		 		if hasattr(inst, 'op2'):
+		 			live_set.add(inst.op2)
+		 	else:
+		 		block.insts.remove(inst)
+		 		removal = True
+
+ 	block.live_in = copy.copy(live_set)
+ 	return removal
+
+
+# Percolate live_out set up the block
+def percolate(block):
+	change = False
+
+	# Copy so that we don't overwrite live_out
+	live_set = copy.copy(block.live_out)
+
+ 	for inst in reversed(block.insts):
+ 		# Remove assignee from live set
+ 		if hasattr(inst, 'assignee'):
+ 			live_set.discard(inst.assignee)
+
+	 		# Add operands to live set
+	 		if hasattr(inst, 'op1'):
+	 			live_set.add(inst.op1)
+	 		if hasattr(inst, 'op2'):
+	 			live_set.add(inst.op2)
+
+ 	if block.live_in != live_set:
+ 		change = True
+
+ 	block.live_in = live_set
+ 	return change
+
+def liveness(blocks):
+
+	change = True # Whether we removed something on last iteration
+	while (change):
+		change = False
+
+		# Compute/propogate liveness set changes until no change
+		for block in blocks:
+
+			# live_out = live_in1 U live_in2 U ...
+			for child in block.children:
+				block.live_out = block.live_out.union(child.live_in)
+
+			# percolate the live_out changes
+
+			change = change or percolate(block)
+
+
+def read_tac(filename):
+	f = open(filename)
+	tac = []
+	for line in f:
+		tac.append(line.rstrip('\n').rstrip('\r'))
+	f.close()
+	return tac
+
 
 def main():
 	filename = sys.argv[1]
@@ -419,20 +499,21 @@ def main():
 	insts = make_inst_list(tac)
 	blocks = make_bbs(insts)
 
-	# print ''
-	# for block in blocks:
-	# 	print (str(block) + '\n')
+	# Dead code elimination
+	removal = True
+	while (removal):
+		removal = False
 
-	# Eliminate dead code
-	
-	changed = True
-	while (changed):
-		changed = False
+		# Refreshes liveness sets of all blocks
+		liveness(blocks)
 
-		# 1. Compute liveness for each basic block
+		# Remove dead code
 		for block in blocks:
+			removal = (removal or kill_dead_code(block))
 
-
+	for block in blocks:
+		for inst in block.insts:
+			print inst
 
 
 if __name__ == '__main__':
